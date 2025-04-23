fc1786e0fce3870f402454fb97a127cb777bc1cd
Fernando Karnagi <fkarnagi@gmail.com>
2025-04-24 06:33:55 +0700

    Update

--- aggregation_pipeline/query_aggregate.js
+++ aggregation_pipeline/query_aggregate.js
@@ -10,60 +10,21 @@ const main = async () => {
 
     const coll = client.db("merchant").collection("customer_transaction_histories");
 
-    var stage1 = { $match: { saleDate: { $gt: new Date(t0) } } }
-
-    // Add 'total' and 'item_count' fields...
-    var stage2 = {
-        $addFields: {
-            total: {
-                $sum: {
-                    $map: {
-                        input: "$items",
-                        as: "row",
-                        in: { $multiply: ["$$row.price", "$$row.quantity"] }
-                    }
-                }
-            },
-            item_count: {
-                $sum: { $size: "$items" }
+    const pipeline = [
+        {
+            $match: {
+                amount: { $gte: 250 },
+                customer_id: '123456789'
             }
-        }
-    }
+        },
+        { $project: { _id: 0, customer_id: 0 } },
+        { $sort: { amount: -1 } },
+        { $limit: 5 }
+    ]
 
-    // Calculate average number of items and total sales per month by location and channel
-    var stage3 = {
-        $group: {
-            _id: {
-                year: { $year: "$saleDate" },
-                month: { $month: "$saleDate" },
-                location: "$storeLocation",
-                channel: "$purchaseMethod"
-            },
-            avg_count: { $avg: "$item_count" },
-            sales: { $sum: "$total" },
-            last_sale: { $max: "$saleDate" }
-        }
-    }
+    const params = { maxTimeMS: 60000, allowDiskUse: true }
 
-    // Record the update time
-    var stage4 = { $addFields: { 'updateTime': now } }
-
-    // Sort by sale date, location and channel
-    var stage5 = { $sort: { _id: 1 } }
-
-    // Merge into report materialized view
-    var stage6 = {
-        $merge: {
-            into: "report",
-            on: "_id",
-            whenMatched: "merge",
-            whenNotMatched: "insert"
-        }
-    }
-
-    var res = await coll.aggregate([stage1, stage2, stage3, stage4, stage4, stage5, stage6])
-        .toArray().then(console.log("Finished merge pipeline..."));
-
+    var res = await coll.aggregate(pipeline, params).toArray();
     console.log(res);
 
     await client.close();
